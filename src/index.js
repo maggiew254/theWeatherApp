8ca5c6dba37dbf3aa207c41f92bb72f80ec411d0
Maggie <114643097+maggiew254@users.noreply.github.com>
2025-10-30 14:12:59 +0300

    created search for temperature

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
+function updateWeather(response) {
+  let temp = Math.round(response.data.temperature.current);
+  let cityTemp = document.querySelector(".weather-temp");
+  cityTemp.innerHTML = temp;
+}
+function searchCity(city) {
+  let apiKey = "24a6f34aet30b286677e3ofdb3c4aea0";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(updateWeather);
+}
 function search(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-input");
   let city = document.querySelector("#weather-city");
   city.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
